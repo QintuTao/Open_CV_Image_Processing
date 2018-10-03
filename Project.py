4a83510a37bb6d0595549d2c12f1a0f6ce2e6ff2
Qintu <tqt010114@icloud.com>
2018-10-03 12:39:23 -0700

    part#2
    - add testing image
    - add user guide
    - add watiKeya()

--- Project.py
+++ Project.py
@@ -14,13 +14,22 @@ print("Please enter the image you want to use[must in the same folder]")
 isValid = False
 while isValid == False:
     user_input = input()
-    if cv2.imread(user_input) == None:
-        print("Invalid input. Try again or enter 'Exit' to exit the program'")
-    elif user_input = "Exit":
+    if cv2.imread(user_input) is not None and user_input != "Exit":
+        isValid = True
+    elif user_input == "Exit":
         exit()
     else:
-        isValid = True
-
+        print("Invalid input. Try again or enter 'Exit' to exit the program'")
 #showing the original image
 img = cv2.imread(user_input)
 cv2.imshow("processing window",img)
+
+#showing user guide
+print("User Mannual\n")
+print("===============================================================================")
+print("1.Press \"H\" to switch the image to hsv mode\n")
+print("2.Enter \"I\" to print basic image info \n")
+print("3.Press \"R\" key to rotate 90 degrees to left")
+
+# tracking user input (in forms of Key_press)
+key = cv2.waitKey(0)
