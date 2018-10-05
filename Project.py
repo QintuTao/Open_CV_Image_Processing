5bbd505a9a8459057788e00c8ef7d09ad36b8a46
Qintu <tqt010114@icloud.com>
2018-10-05 08:12:18 -0700

    part#3
    I key

--- Project.py
+++ Project.py
@@ -28,13 +28,25 @@ cv2.imshow("processing window",img)
 print("User Mannual\n")
 print("=====================================================================================")
 print("1.Press \"H\" key to switch the image to hsv mode\n")
-print("2.Enter \"I\" key to print basic image info \n")
-print("3.Press \"R\" key key to rotate 90 degrees to left\n")
+print("2.Press \"I\" key to print basic image info \n")
+print("3.Press \"L\" key key to rotate 90 degrees to left\n")
+print("4.Press \"R\" key to filter red;\n Press \"B\" key to filter blue;\n Press")
 
 # tracking user input (in forms of Key_press)
 key = cv2.waitKey(0)
     #turn the image to hsv mode
 if key == ord("h"):
-
+    im
     #basic info of image
 elif key == ord("i"):
+    img_gray = cv2.imread(user_input,0)
+    row,col = img_gray.shape
+    row,col,color = img.shape
+    print("The size of the is {0} x {1} big".format(row,col))
+
+
+#function that shows the resultant image
+def showResult(img):
+    cv2.imshow("result",img)
+    k = cv2.waitKey(0)
+    print("Press esc to exit")
